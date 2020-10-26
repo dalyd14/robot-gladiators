2bdf8c1efcec9881730871c3af0fad4e91cfb318
David Daly <dalyd14@gmail.com>
2020-10-26 18:08:47 -0400

    changed so that you can enter a number into the shop() function

--- assets/js/game.js
+++ assets/js/game.js
@@ -116,18 +116,18 @@ var endGame = function() {
 
 var shop = function() {
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store?" +
-        "Please enter one: REFILL, UPGRADE, or LEAVE to make a choice."
+        "Would you like to 1: REFILL your health, 2: UPGRADE your attack, or 3: LEAVE the store?" +
+        "Please enter one: 1 to REFILL, 2 to UPGRADE, or 3 to LEAVE to make a choice."
     )
 
-    switch(shopOptionPrompt.toUpperCase()) {
-        case "REFILL":
+    switch(parseInt(shopOptionPrompt)) {
+        case 1:
             playerInfo.refillHealth()
             break;
-        case "UPGRADE":
+        case 2:
             playerInfo.upgradeAttack()
             break;
-        case "LEAVE":
+        case 3:
             window.alert("Leaving the store.")
             break;
         default:
